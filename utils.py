a61bb4407d30bdbc092639ee1a3ebc32c42f8a9f
Yewen <yewenzhou@berkeley.edu>
2022-11-04 21:59:17 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,10 @@ from skimage.color import rgb2gray
 
 
 INPUT_SIZE = 299
-STRIDE = 128
+TRAIN_STRIDE = 128
+TEST_STRIDE = 128
+
+TRAIN_SAMPLE_SIZE = 500
 
 # define input paths
 train_input_im = "tumor_091.tif"
@@ -18,19 +21,25 @@ train_input_mask = "tumor_091_mask.tif"
 level1 = 4
 level2 = 5
 
-# define training output paths
-zoom1_tumor_patches = "./training/zoom1/patches/tumor"
-zoom1_normal_patches = "./training/zoom1/patches/normal"
-zoom1_tumor_masks = "./training/zoom1/masks/tumor"
-zoom1_normal_masks = "./training/zoom1/masks/normal"
+training_dir = "./training"
+zoom1_patches = "./training/zoom1/patches"
+zoom2_patches = "./training/zoom2/patches"
 
-zoom2_tumor_patches = "./training/zoom2/patches/tumor"
-zoom2_normal_patches = "./training/zoom2/patches/normal"
-zoom2_tumor_masks = "./training/zoom2/masks/tumor"
-zoom2_normal_masks = "./training/zoom2/masks/normal"
+# define training output paths
+zoom1_tumor_patches = f"{training_dir}/zoom1/patches/tumor"
+zoom1_normal_patches = f"{training_dir}/zoom1/patches/normal"
+zoom1_tumor_masks = f"{training_dir}/zoom1/masks/tumor"
+zoom1_normal_masks = f"{training_dir}/zoom1/masks/normal"
+
+zoom2_tumor_patches = f"{training_dir}/zoom2/patches/tumor"
+zoom2_normal_patches = f"{training_dir}/zoom2/patches/normal"
+zoom2_tumor_masks = f"{training_dir}/zoom2/masks/tumor"
+zoom2_normal_masks = f"{training_dir}/zoom2/masks/normal"
 
 # seed for ImageDataGenerator
 seed = 100
+
+train_batch_size = 16
 
 def read_slide(slide, x, y, level, width, height, as_float=False):
     """Read a region from the slide, return a numpy RGB array
@@ -81,10 +90,16 @@ def cal_tissue_perc(image, intensity=0.8) -> float:
 
 
 def extract_label(im, mask) -> int:
-    """Extract label (0/1) from the center 128 x 128 of the im"""
-    start_i = im.shape[0] // 2 - STRIDE // 2
-    end_i = start_i + STRIDE
-    start_j = im.shape[1] // 2 - STRIDE // 2
-    end_j = start_j + STRIDE
+    """Extract label (0/1) from the center 128 x 128 of the im
+    
+    Args:
+        im: 3D array, (299, 299, 3)
+        mask: 3D array, (299, 299, 3)
+    """
+    start_i = im.shape[0] // 2 - TEST_STRIDE // 2
+    end_i = start_i + TEST_STRIDE
+    start_j = im.shape[1] // 2 - TEST_STRIDE // 2
+    end_j = start_j + TEST_STRIDE
+    mask = mask[:, :, 0]
     mask_slice = mask[start_i:end_i, start_j:end_j]
     return int(np.sum(mask_slice) >= 1)
